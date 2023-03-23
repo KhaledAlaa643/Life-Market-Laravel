aa2e81e5b4a7bb67dcbf23da3dbfddc98bd9d170
walid <walidmostafa209@gmail.com>
2023-03-23 13:18:50 +0200

    modify contact us table

--- database/migrations/2023_03_15_212340_create_contact_us_table.php
+++ database/migrations/2023_03_15_212340_create_contact_us_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("user_name");
             $table->string("email");
-            $table->string("comment");
+            $table->text("comment");
 
             $table->timestamps();
         });
