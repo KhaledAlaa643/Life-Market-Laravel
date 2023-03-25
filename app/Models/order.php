8170bafedcb77629f775687df002c66395adc833
walid <walidmostafa209@gmail.com>
2023-03-25 13:47:32 +0200

    modify order route

--- app/Models/order.php
+++ app/Models/order.php
@@ -1,13 +1,6 @@
 <?php
 namespace App\Models;
-<<<<<<< HEAD
-
 use App\Models\User;
-
-
-=======
-use App\Models\User;
->>>>>>> aml
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\order_items;
@@ -21,20 +14,9 @@ class order extends Model
         return $this->hasMany(order::class);
     }
 
-<<<<<<< HEAD
-=======
-
-
-    
->>>>>>> aml
     public function user (){
 
         return $this->belongsTo(User::class);
     }
 
 }
-<<<<<<< HEAD
-
-=======
-}
->>>>>>> aml
